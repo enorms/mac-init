20123fb9fa8e03de0ad72e63339ebf248d05e63d
Eric Norman <7156025+lifekaizen@users.noreply.github.com>
2019-02-17 16:27:19 -0800

    Add macOS prefs.
    referencing https://github.com/mathiasbynens/dotfiles/blob/e72d1060f3df8c157f93af52ea59508dae36ef50/.macos

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -137,6 +137,23 @@ function configure_finder() {
     # Always show scrollbars
     defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
     # Possible values: `WhenScrolling`, `Automatic` and `Always`
+    
+    # Save to disk (not to iCloud) by default
+    defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+    
+    # Automatically quit printer app once the print jobs complete
+    defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+    
+    # Disable the “Are you sure you want to open this application?” dialog
+    # defaults write com.apple.LaunchServices LSQuarantine -bool false
+    
+    # Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
+    sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+    
+    # Restart automatically if the computer freezes
+    sudo systemsetup -setrestartfreeze on
+
+
 }
 
 function quit() {
