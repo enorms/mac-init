4c41e10f3de102294a6a0db40e25f3ff8361b8dd
Eric Norman <eric@norman.zone>
2021-06-23 09:51:47 -0700

    Add application specific settings

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -1,15 +1,15 @@
-# Terminal commands are a fast and precise way to customize all the little settings
+#!/usr/bin/env zsh
+  
+
+###########################################################################################
+#
+# Example to look at one value:
+#   Check:
+#     % defaults read NSGlobalDomain AppleShowScrollBars
+#     Always
 # 
-# To apply all, execute as shell program.
-# 
-# To specify one, use terminal directly as follows.
-# 
-# Check current setting:
-# (base) eric@mbp-16 mac-init % defaults read NSGlobalDomain AppleShowScrollBars
-# Always
-# 
-# Set:
-# defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+#   Set:
+#     defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 # 
 # 
 # References
@@ -17,10 +17,9 @@
 # https://www.learningosx.com/101-ways-to-tweak-os-x-using-terminal/
 # https://www.cnet.com/news/check-defaults-before-altering-hidden-settings-in-os-x/
 # https://superuser.com/questions/427154/reset-mac-os-x-terminal-to-default/448679
-# 
-
-
-#!/usr/bin/env bash
+#
+#
+###########################################################################################
 
 
 main() {
@@ -48,10 +47,27 @@ function configure_mac() {
     while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
+    ##############################################################################
+    # Security                                                                   #
+    ##############################################################################
+    # More options here: https://github.com/atomantic/dotfiles/blob/master/install.sh
+    # Based on:
+    # https://github.com/drduh/macOS-Security-and-Privacy-Guide
+    # https://benchmarks.cisecurity.org/tools2/osx/CIS_Apple_OSX_10.12_Benchmark_v1.0.0.pdf
 
-###############################################################################
-# For coding                                                                  #
-###############################################################################
+    # Enable firewall. Possible values:
+    #   0 = off
+    #   1 = on for specific sevices
+    #   2 = on for essential services
+    sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+
+    # Enable firewall stealth mode (no response to ICMP / ping requests)
+    # Source: https://support.apple.com/kb/PH18642
+    sudo defaults write /Library/Preferences/com.apple.alf stealthenabled -int 1
+
+    ###############################################################################
+    # For coding                                                                  #
+    ###############################################################################
 
     # Disable automatic capitalization as it’s annoying when typing code
     defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
@@ -69,9 +85,9 @@ function configure_mac() {
     # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
     defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
 
-###############################################################################
-# General UI/UX                                                               #
-###############################################################################
+    ###############################################################################
+    # General UI/UX                                                               #
+    ###############################################################################
 
     ## Enable tap to click
     defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
@@ -110,24 +126,20 @@ function configure_mac() {
     # Disable the Launchpad gesture (pinch with thumb and three fingers)
     defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 
-    ## Automatically hide and show the Dock
-    # defaults write com.apple.dock autohide -bool true
-    ## Remove the auto-hiding Dock delay
-    # defaults write com.apple.dock autohide-delay -float 0    
-    
+  
     ## Hot corners
-    ## Possible values:
-    ##  0: no-op
-    ##  2: Mission Control
-    ##  3: Show application windows
-    ##  4: Desktop
-    ##  5: Start screen saver
-    ##  6: Disable screen saver
-    ##  7: Dashboard
-    ## 10: Put display to sleep
-    ## 11: Launchpad
-    ## 12: Notification Center
-    ## Top left screen corner
+    # Possible values:
+    #  0: no-op
+    #  2: Mission Control
+    #  3: Show application windows
+    #  4: Desktop
+    #  5: Start screen saver
+    #  6: Disable screen saver
+    #  7: Dashboard
+    # 10: Put display to sleep
+    # 11: Launchpad
+    # 12: Notification Center
+    # Top left screen corner
     defaults write com.apple.dock wvous-tl-corner -int 0
     defaults write com.apple.dock wvous-tl-modifier -int 0
     ## Top right screen corner
@@ -137,15 +149,16 @@ function configure_mac() {
     defaults write com.apple.dock wvous-bl-corner -int 0
     defaults write com.apple.dock wvous-bl-modifier -int 0
 
+
     open "Dock"
 
 
     ## Save screenshots to Downloads folder
-   defaults write com.apple.screencapture location -string "${HOME}/Downloads"
+    defaults write com.apple.screencapture location -string "${HOME}/Downloads"
 
-## Require password immediately after sleep or screen saver begins
-    # defaults write com.apple.screensaver askForPassword -int 1
-    # defaults write com.apple.screensaver askForPasswordDelay -int 0
+    ## Require password immediately after sleep or screen saver begins
+    defaults write com.apple.screensaver askForPassword -int 1
+     defaults write com.apple.screensaver askForPasswordDelay -int 0
 
     ## allow quitting via ⌘ + q; doing so will also hide desktop icons
     defaults write com.apple.finder QuitMenuItem -bool true
@@ -158,14 +171,11 @@ function configure_mac() {
     defaults write com.apple.finder NewWindowTargetPath -string \
         "file://${HOME}/Downloads/"
 
-    ## disable status bar
-    # defaults write com.apple.finder ShowStatusBar -bool false
-
     ## Enable/Disable Path Bar on bottom of Finder windows
-   defaults write com.apple.finder ShowPathbar -bool true
+    defaults write com.apple.finder ShowPathbar -bool true
 
     ## Display full POSIX path as Finder window title
-     defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
     ## Keep folders on top when sorting by name
     defaults write com.apple.finder _FXSortFoldersFirst -bool true
@@ -186,9 +196,6 @@ function configure_mac() {
     
     # Automatically quit printer app once the print jobs complete
     defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-    
-    # Disable the “Are you sure you want to open this application?” dialog
-    #  defaults write com.apple.LaunchServices LSQuarantine -bool false
 
     # Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
     sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
@@ -213,6 +220,110 @@ function configure_mac() {
 
     # Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
     /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+
+
+    ###############################################################################
+    # Application Specific                                                        #
+    ###############################################################################
+
+    ## Google Chrome
+    # Disable the all too sensitive backswipe on trackpads
+    defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+    defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+    # Disable the all too sensitive backswipe on Magic Mouse
+    defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+    defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+
+    # Use the system-native print preview dialog
+    defaults write com.google.Chrome DisablePrintPreview -bool true
+    defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+    # Expand the print dialog by default
+    defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+    defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+    
+    ## SizeUp
+    # Start SizeUp at login
+    defaults write com.irradiatedsoftware.SizeUp StartAtLogin -bool true
+
+    # Don’t show the preferences window on next start
+    defaults write com.irradiatedsoftware.SizeUp ShowPrefsOnNextStart -bool false
+    
+    ## Mac App
+    
+    # Allow the App Store to reboot machine on macOS updates
+    defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+
+    # Enable the WebKit Developer Tools in the Mac App Store
+    defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+    # Enable Debug Menu in the Mac App Store
+    defaults write com.apple.appstore ShowDebugMenu -bool true
+
+    # Enable the automatic update check
+    defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+    # Check for software updates daily, not just once per week
+    defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+    # Download newly available updates in background
+    defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+    # Turn on app auto-update
+    defaults write com.apple.commerce AutoUpdate -bool true
+
+    ## Address Book, Dashboard, iCal, TextEdit, and Disk Utility
+
+    # Enable the debug menu in Address Book
+    defaults write com.apple.addressbook ABShowDebugMenu -bool true
+
+    # Enable Dashboard dev mode (allows keeping widgets on the desktop)
+    defaults write com.apple.dashboard devmode -bool true
+
+    # Use plain text mode for new TextEdit documents
+    defaults write com.apple.TextEdit RichText -int 0
+    # Open and save files as UTF-8 in TextEdit
+    defaults write com.apple.TextEdit PlainTextEncoding -int 4
+    defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+    # Enable the debug menu in Disk Utility
+    defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+    defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+    # Activity Monitor
+
+    # Show the main window when launching Activity Monitor
+    defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+    # Visualize CPU usage in the Activity Monitor Dock icon
+    defaults write com.apple.ActivityMonitor IconType -int 5
+
+    # Show all processes in Activity Monitor
+    defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+    # Sort Activity Monitor results by CPU usage
+    defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+    defaults write com.apple.ActivityMonitor SortDirection -int 0
+    
+    ## Time Machine
+
+    # Prevent Time Machine from prompting to use new hard drives as backup volume
+    defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+    
+    ## Terminal
+    
+    # Disable the annoying line marks (0)
+    defaults write com.apple.Terminal ShowLineMarks -int 1
+
+    # Only use UTF-8 in Terminal.app
+    defaults write com.apple.terminal StringEncodings -array 4
+    
+    # Enable Secure Keyboard Entry in Terminal.app
+    # See: https://security.stackexchange.com/a/47786/8918
+    defaults write com.apple.terminal SecureKeyboardEntry -bool true
+
 
 
 
